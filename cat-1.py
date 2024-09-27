c80e7f5639dce9b1c6989759bbbd0df384b07eec
mwangi James <mwangi.jamo@outlook.com>
2024-09-27 10:35:49 +0300

    python cat

---
+++ cat-1.py
@@ -0,0 +1,13 @@
+print("Hello, world!")
+
+# Function add-numbers
+
+def add_numbers(num1, num2):
+    result = num1 + num2
+    print(f"The sum of {num1} and {num2} is {result}")
+
+    # Function square
+
+    def square(num):
+        result = num ** 2
+        print(f"The square of {num} is {result}")
